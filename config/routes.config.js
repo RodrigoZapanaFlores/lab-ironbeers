ec21c6383dd0e88f0f6e41439e26a429ddea58f9
unknown <rodrigozapanaflores@gmail.com>
2022-07-07 16:02:20 +0200

    done

---
+++ config/routes.config.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const misc = require('../controllers/misc.controller');
+const beers = require('../controllers/beers.controller');
+
+router.get('/', misc.home);
+router.get('/beers', beers.list);
+router.get('/beers/:id', beers.detail)
+router.get('/random-beer', beers.random);
+
+module.exports = router;
---
+++ controllers/beers.controller.js
@@ -0,0 +1,32 @@
+const PunkApi = require('../config/punk.config');
+
+module.exports.list = (req, res, next) => {
+  PunkApi.getBeers()
+    .then(beers => {
+      beers = beers.map(beer => {    //crea un map para tener los 100 caracteres
+        beer.short_description = beer.description.substring(0, 100);
+        return beer;
+      })
+      res.render('beers/list', { beers })
+    })
+    .catch(error => next(error));
+}
+
+module.exports.random = (req, res, next) => {
+  PunkApi.getRandom()
+    .then(([beer]) => res.render('beers/detail', { beer }))
+    .catch(error => next(error));
+}
+
+module.exports.detail = (req, res, next) => {
+  const { id } = req.params;
+  PunkApi.getBeer(id)
+    .then((beers) => {
+      if (beers && beers.length > 0) {
+        res.render('beers/detail', { beer: beers[0] })
+      } else {
+        res.redirect('/beers');
+      }
+    })
+    .catch(error => next(error));
+}
